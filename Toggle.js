92af2d6c8cc600b5bf9fad30fc3f91cb7211b30b
Your Name <lindazoghlami@gmail.com>
2022-04-01 05:59:26 +0100

    new changes

--- Toggle.js
+++ Toggle.js
@@ -1,8 +1,13 @@
-
-
-
-
-var min = document.getElementById("minus-icon");
-min.onclick = function(min){
-    min.target.classList.toggle('.minuss-icon')
+let image = document.getElementById('minusicon')
+let hide = document.getElementById('hide')
+function change(){
+    
+    // if (image.src.match('fig4.svg')){
+    //     image.src = 'fig4.svg';
+    // } else
+    //    { image.src = 'fig8.svg'; 
+    //     hide.remove()}
+    
+    image.src = 'fig8.svg'; 
+        hide.remove()
 }
